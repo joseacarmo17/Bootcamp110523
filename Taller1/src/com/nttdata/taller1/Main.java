15efc36dc8ca71eeae6c8d2b5e7911cb4a4fa1b6
Jose Antonio Carmona <jcarmond@emeal.nttdata.com>
2023-05-11 08:52:12 +0200

    Cambio del resultado taller 1

--- Taller1/src/com/nttdata/taller1/Main.java
+++ Taller1/src/com/nttdata/taller1/Main.java
@@ -8,16 +8,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(18, 6, 4, 15, 55, 78, 12, 9, 8);
-		
+		int count = 0;
 		//Metodo imperativo
 		for(int number: numbers) {
 			if(number>10) {
-				System.out.println(number);
+				count++;
 			}
 		}
+		System.out.println(count);
 		
 		//Metodo funcional
-		System.out.println(numbers.stream().filter(number -> number>10).collect(Collectors.toList()));
+		System.out.println(numbers.stream().filter(number -> number>10).count());
 	}
 
 }
